f4a22ce06d54cd6799b4285316c8da1057ed4bd7
Hack Reactor Students <accounts@hackreactor.com>
2015-09-24 14:47:23 -0700

    (feat) Implement spacePressHandler

--- client/views/InputView.js
+++ client/views/InputView.js
@@ -20,7 +20,6 @@ var InputView = Backbone.View.extend({
     var currentInput = e.target.value;
     if(currentInput[currentInput.length-1] === " "){
       this.model.set('currentWord', currentInput.substring(0, currentInput.length-1));
-
       e.target.value = "";
     }
   }
