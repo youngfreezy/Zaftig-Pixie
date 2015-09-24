9713533911181a24c0adb8a3c62723594b069ca5
Manningham <Nstarun@gmail.com>
2015-09-23 19:56:55 -0700

    (feat) Added basic server routing capability

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var db = require('./db');
 
 // Middleware
 var parser = require('body-parser');
@@ -8,17 +7,12 @@ var parser = require('body-parser');
 var router = require('./routes.js');
 
 var app = express();
-module.exports.app = app;
 
 // Set what we are listening on.
 app.set("port", 3000);
 
 // Logging and parsing
-app.use(morgan('dev'));
 app.use(parser.json());
-
-// Set up our routes
-app.use("/classes", router);
 
 // Serve the client files
 app.use(express.static(__dirname + "/../client"));
@@ -28,3 +22,10 @@ if (!module.parent) {
   app.listen(app.get("port"));
   console.log("Listening on", app.get("port"));
 }
+
+/*----------  Routes  ----------*/
+
+
+app.get('/test', function (request, response) {
+  console.log('Test totally works, biatch!');
+});
