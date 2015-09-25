d54a206c0e50ac85d00142bda73bdd55207151fb
Manningham <Nstarun@gmail.com>
2015-09-25 12:04:44 -0700

    (fix) Add package.json file to server

--- server/request-handlers.js
+++ server/request-handlers.js
@@ -1,6 +1,7 @@
 var db = require('./config');
 var bodyParser = require('body-parser');
 var User = require('./mongoModels/user');
+var req = require('request');
 // var utility = require('./utility');
 
 module.exports.user = function (request, response) {
@@ -55,19 +56,33 @@ module.exports.register = function (request, response) {
 
 module.exports.text = function (request, response) {
 
-  var data = {}
-  data.text = "Face it, Nate, you are about the greatest thing since pizza. No one else can hoist like you can. \
-  Your best friend says your the green-est person in the world. Sure, you once got drunk and lost your phone in the oven, \
-  but you also... I don\'t know, worked at an orphanage? So, buttercup, today is the day you’re going to stop beating yourself \
-  up for being sucky and start loving yourself for being happy, joyous, and elated. And if people give you a festering time, \
-  just tell ‘em they can take their hair and squat it up their cornea!"
+  // data.text = "Face it, Nate, you are about the greatest thing since pizza. No one else can hoist like you can. \
+  // Your best friend says your the green-est person in the world. Sure, you once got drunk and lost your phone in the oven, \
+  // but you also... I don\'t know, worked at an orphanage? So, buttercup, today is the day you’re going to stop beating yourself \
+  // up for being sucky and start loving yourself for being happy, joyous, and elated. And if people give you a festering time, \
+  // just tell ‘em they can take their hair and squat it up their cornea!"
   
-  console.log('in server text method');
-  if(request.method === "GET"){
+  // console.log('in server text method');
+  // if(request.method === "GET"){
+  //   response.send(data);
+  // }
+
+  req('http://api.icndb.com/jokes/random/6', function (error, res, body) {
+    console.log('\n\nRequest module called to access jokes API.\n\n');
+    var data = {}
+    var jokes = [];
+    var parsedBody = JSON.parse(body);
+    parsedBody.value.forEach(function(jokeObject) {
+      jokes.push(jokeObject.joke);
+    });
+    data.text = jokes.join(' ');
     response.send(data);
-  }
+
+  });
+
 
 
 };
+
 
 
