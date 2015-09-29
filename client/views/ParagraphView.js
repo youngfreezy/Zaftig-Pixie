3bde913a11c8fa1e575a4af7454c54ed94964010
Kevin Nevalsky <knevalsky@gmail.com>
2015-09-28 17:20:07 -0700

    (feat) Created stats view container

--- client/views/ParagraphView.js
+++ client/views/ParagraphView.js
@@ -5,21 +5,43 @@ var ParagraphView = Backbone.View.extend({
   className: "paragraphText",
 
   initialize: function () {
-    this.model.on('change:currentIndex', function () {
-      this.render();
-    }, this);
-
+    this.model.on('change:currentIndex', this.updateCurrent, this);
+    
     this.render();
   },
 
   render: function () {
+    // return this.$el.html([
+    //   this.model.get('paragraphArray')[this.model.get('currentIndex')] + " ",
+    //   this.model.get('paragraphArray')[this.model.get('currentIndex') + 1] + " ",
+    //   this.model.get('paragraphArray')[this.model.get('currentIndex') + 2] + " ",
+    //   this.model.get('paragraphArray')[this.model.get('currentIndex') + 3] + " ",
+    //   this.model.get('paragraphArray')[this.model.get('currentIndex') + 4]
+    //   ]);
     return this.$el.html([
-      this.model.get('paragraphArray')[this.model.get('currentIndex')] + " ",
-      this.model.get('paragraphArray')[this.model.get('currentIndex') + 1] + " ",
-      this.model.get('paragraphArray')[this.model.get('currentIndex') + 2] + " ",
-      this.model.get('paragraphArray')[this.model.get('currentIndex') + 3] + " ",
-      this.model.get('paragraphArray')[this.model.get('currentIndex') + 4]
+      "<p>" + this.model.get('currentLine').join(" ") + "</p>",
+      "<p>" + this.model.get('nextLine').join(" ") + "</p>"
+      ]);
+  }, 
+
+  updateLines: function () {
+    this.model.updateCurrentLine();
+    this.model.updateNextLine();
+  }, 
+
+  updateCurrent: function () {
+    var index = this.model.get('currentIndex');
+    var lineIndex = index % 5;
+    if (lineIndex % 5 === 0) {
+      this.updateLines();
+    }
+    var currentLine = this.model.get('currentLine').slice();
+    var nextLine = this.model.get('nextLine').slice();
+
+    currentLine[lineIndex] = "<span class='currentWord'>" + currentLine[lineIndex] + "</span>"
+    this.$el.html([
+      "<p>" + currentLine.join(" ") + "</p>",
+      "<p>" + nextLine.join(" ") + "</p>"
       ]);
   }
-
 });
---
+++ client/views/StatsView.js
@@ -0,0 +1,21 @@
+var StatsView = Backbone.View.extend({
+  
+  tagName: "div",
+
+  className: "statsView",
+
+  initialize: function ( params ) {
+    this.model.on('update', this.render, this);
+    console.log("initalize");
+    this.render();
+  },
+
+  render: function () {
+    console.log('wpm changed');
+    return this.$el.html([
+      'wpm: ' + Math.floor(this.model.get('wpm')),
+      ', correct: ' + this.model.get('numCorrect'),
+      ', missed: ' + this.model.get('numMissed')
+    ]);
+  }
+});
