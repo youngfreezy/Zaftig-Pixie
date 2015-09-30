41d06c17d3c8916a2e0e60707ae1091f89899a4f
Manningham <Nstarun@gmail.com>
2015-09-29 21:54:34 -0700

    (feat) Refactor server to handle sockets, implement server cache to store server-side session data, bind sockets to proper server cache data using pointers, implement server-side 'login' event listeners and functionality, WIP implement server-side 'update' event listeners and event handler functionality

--- server/server.js
+++ server/server.js
@@ -21,19 +21,41 @@ app.use(express.static(__dirname + "/../client"));
 
 // If we are being run directly, run the server.
 if (!module.parent) {
-  app.listen(app.get("port"));
+  http.listen(app.get("port"));
   console.log("Listening on", app.get("port"));
 }
+
+/*----------  Server Cache  ----------*/
+
+var users = {};
+var numberOfUsers = 0;
 
 /*----------  Socket listeners  ----------*/
 
 io.on('connection', function (socket) {
 
+  socket.on('login', function() {
+    socketHandlers.loginUser(socket);
+    numberOfUsers++;
+    console.log('There are ' + numberOfUsers + ' users connected.');
+    console.log('This is the users object: ', users);
+  });
+
   socket.on('update', function (data) {
+    // console.log('Update fired, this is the score: \n\n\n', socket.userData);
     // update the other users with this user's data
-    socket.broadcast.emit('update', data);
+    socketHandlers.updateScore(socket, data, function (socket) {
+      // save the result of checkForEndGame to see if we will emit an update event or not
+      var endGameStatus = socketHandlers.checkForEndGame(socket);
+      console.log('This is the endGameStatus: ', endGameStatus);
+      if (endGameStatus === 'noWinner') {  // if there was no winner, 
+        // update the opponent with this socket's score
+        socket.broadcast.emit('update', data);
+      }
+    });
   });
   
+
     /* THIS LOGIC WILL BE IMPLEMENTED CLIENT SIDE?*/
   // socket.on('endGame', function (data) {
   //   // update the other users with the end game results
@@ -58,3 +80,6 @@ app.use('/register', handlers.register);
 
 // serve passage to the client
 app.use('/text', handlers.text);
+
+module.exports.users = users;
+module.exports.numberOfUsers = numberOfUsers;
