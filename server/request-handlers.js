9a903a03d045f8463789becd251bb3ff7bde3ace
Manningham <Nstarun@gmail.com>
2015-10-01 09:44:59 -0700

    (Fix) add formatString() to .text() request handler method to correct errors in API output

--- server/request-handlers.js
+++ server/request-handlers.js
@@ -54,7 +54,30 @@ module.exports.register = function (request, response) {
 };
 
 module.exports.text = function (request, response) {
+  // Correct errors in API string output before serving back to client
+  var formatString = function (string) {
+    var result = ''
+    // replace "&quot;" with ""
+    var escapedQuotes = string.replace(/&quot;/g, '"');
+    // replace out-of-place "?"s with "'"s
+    for (var i = 0; i < escapedQuotes.length; i++) {
+      if (escapedQuotes[i] === "?") {
+        if (escapedQuotes[i-1] === "s" && escapedQuotes[i+1] === "a") {
+          // handle one-off typo on API return object id# 283
+          result += '... ';   
+        } else if (escapedQuotes[i+1] !== " " && escapedQuotes[i+1] !== '"') {
+          result += "'";
+        } else {
+          result += escapedQuotes[i];
+        }
+      } else {
+        result += escapedQuotes[i];
+      }
+    }
 
+    return result;
+  }
+  // Hit API for data, correct errors, serve to client
   req('http://api.icndb.com/jokes/random/6', function (error, res, body) {
     var data = {}
     var jokes = [];
@@ -63,6 +86,7 @@ module.exports.text = function (request, response) {
       jokes.push(jokeObject.joke);
     });
     data.text = jokes.join(' ');
+    data.text = formatString(data.text);
     response.send(data);
   });
 
