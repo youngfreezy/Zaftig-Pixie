e03d63c244970a73d2541870ea7b9f5427018f43
jingpei <jingjpei@gmail.com>
2015-10-03 22:24:44 -0700

    (feat) Add test structure and start gruntfile

---
+++ include.conf.js
@@ -0,0 +1,13 @@
+module.exports = [
+  "./client/d3/raceVisuals.js",
+  "./client/models/AppModel.js",
+  "./client/models/GameDisplayModel.js",
+  "./client/models/SpeedTyperModel.js",
+  "./client/views/AppView.js",
+  "./client/views/StatsView.js",
+  "./client/views/InputView.js",
+  "./client/views/ParagraphView.js",
+  "./client/views/GameDisplayView.js",
+  "./client/views/GameContainerView.js",
+  "./client/views/SpeedTyperView.js"
+];
