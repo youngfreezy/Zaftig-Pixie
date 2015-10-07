2c6dac65bcebba30707a8a98715e4a6d1a600523
Brett Sanders <bsand08@gmail.com>
2015-10-06 19:00:46 -0700

    Add semi-colons and formatting.

--- server/mongoModels/user.js
+++ server/mongoModels/user.js
@@ -14,7 +14,7 @@ User.comparePassword = function(candidatePassword, savedPassword, cb) {
   // console.log('\n\ncandidatePassword = ' + candidatePassword + '\nsavedPassword = ' + savedPassword + '\n\n');
   bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
     if (err) {
-      return cb(err)
+      return cb(err);
     }
     // console.log('This is the value of "isMatch": ',isMatch);
     cb(null, isMatch);
